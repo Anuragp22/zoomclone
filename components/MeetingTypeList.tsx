f5e58e4ce4a6f43089e5fdb354af2f66840b94ca
Anuragp22 <anuragp2003b@gmail.com>
2024-05-08 21:07:08 +0530

    Stream setup, Meeting creation

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,11 +4,65 @@ import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { useUser } from '@clerk/nextjs'
+import { useToast } from './ui/use-toast'
 
 const MeetingTypeList = () => {
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
     const Router = useRouter()
-    const createMeeting = () => {
+    const { user } = useUser()
+    const client = useStreamVideoClient()
+    const [values, setValues] = useState({
+        dateTime: new Date(),
+        description: '',
+        link: '',
+    })
+
+    const [callDetails, setCallDetails] = useState<Call>()
+    const { toast } = useToast()
+    const createMeeting = async () => {
+        if (!client || !user) return;
+
+        try {
+
+            if (!values.dateTime) {
+                toast({
+                    title: "Please select a date and time",
+                })
+                return;
+            }
+
+            const id = crypto.randomUUID();
+            const call = client.call('default', id);
+            if (!call) throw new Error('Failed to create call');
+
+            const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+            const description = values.description || 'Instant Meeting';
+
+            await call.getOrCreate({
+                data: {
+                    starts_at: startsAt,
+                    custom: {
+                        description
+                    }
+                }
+            })
+
+            setCallDetails(call);
+            if (!values.description) {
+                Router.push(`/meeting/${call.id}`)
+            }
+            toast({
+                title: "Meeting created",
+            })
+        } catch (error) {
+            console.log(error)
+            toast({
+                title: "Failed to create meeting",
+            })
+        }
 
     }
     return (
