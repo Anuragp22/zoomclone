f8267d04b8473cbbed86b9ee5642dfd43d395a6f
Anuragp22 <anuragp2003b@gmail.com>
2024-05-11 13:30:28 +0530

    Final Commits

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { useUser } from '@clerk/nextjs'
 import { useToast } from './ui/use-toast'
 import { Textarea } from './ui/textarea'
 import ReactDatePicker from 'react-datepicker'
+import { Input } from './ui/input'
 
 const MeetingTypeList = () => {
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
@@ -127,6 +128,17 @@ const MeetingTypeList = () => {
                 onClose={() => setMeetingState(undefined)}
                 title="Start an Instant Meeting"
                 className='text-center' buttonText='start Meeting' handleClick={createMeeting} />
+            <MeetingModal
+                isOpen={meetingState === 'isJoiningMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Type the link here"
+                className='text-center' buttonText='Join Meeting' handleClick={() => Router.push(values.link)} >
+                <Input
+                    placeholder='Meeting Link' className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+                    onChange={(e) => { setValues({ ...values, link: e.target.value }) }}
+                />
+
+            </MeetingModal>
         </section>
     )
 }
