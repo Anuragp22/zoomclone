d69b94d5a61fc1aaaf37ccfe78806a0febc825e1
Anuragp22 <anuragp2003b@gmail.com>
2024-05-11 12:33:51 +0530

    UpComming Meeting

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const PersonalRoom = () => {
     return (
         <section className='flex size-full flex-col gap-10 text-white'>
-            <h1 className='text-3xl font-bold'>Presonal</h1>
+            <h1 className='text-3xl font-bold'>Personal Room</h1>
+            <div className='flex w-full flex-col gap-8 xl:max-w-[900px]'>
+
+            </div>
         </section>
     )
 }
